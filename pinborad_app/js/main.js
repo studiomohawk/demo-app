8143cf9b25ff17ffcfb3c4bc15682d05fbfa9843
chiiia12 <llnsbchsep12@gmail.com>
2014-06-07 22:48:58 +0900

    add click function

--- pinborad_app/js/main.js
+++ pinborad_app/js/main.js
@@ -1,23 +1,136 @@
 $(function(){
 	var menuType;
-	var recomendUser = ["studiomohawk","studiomohawk","studiomohawk"];
-	var recomendTag = ["javascript","chrome","browser","business"];
-	$('.menu').click(function(){
-		$(this).addClass('menu_active');
-		 menuType = $(this).attr('data-menu');
-		if(menuType!=='popular'){
-			$('.form-group,#recomend-area').css('display','block');
-			if(menuType=="u"){
-				$('#recomend-area > p').html('おすすめのユーザー');
-				for(i=0;i<recomendUser.length;i++){
-					$('li').html(recomendUser[i]).appendTo('#recomend-list');
+	var recomendUser = ["studiomohawk","studiomohawk","studiomohawk"],
+	recomendTag = ["javascript","chrome","browser","business"],
+	tabclicked = {'user':false,'tag':false,'popular':false};
+	console.log(tabclicked['user']);
+
+	clickUser();
+
+	function clickUser(){
+		$('[data-menu]').removeClass('menu_active');
+		$('[data-menu="user"]').addClass('menu_active');
+		$('.tag-content,.popular-content').css('display','none');
+		$('.user-content').css('display','block');
+
+		$('#user-search-btn').click(function(){
+			searchWord = $('#user-search-word').val();
+			$('#user-recomend-area').html('');
+			$('#user-element').html('');
+			$.ajax({
+				type:'GET',
+				url:'http://feeds.pinboard.in/json/u:'+searchWord,
+				dataType:'jsonp',
+				jsonp:'cb',
+				success:function(json){
+
+					for(i=0;i<10;i++){
+						var title = json[i].d,
+						url = json[i].u,
+						disc = json[i].n;
+						$('<div></div>').html('<a href="'+url+'"<h3>'+title+'</h3></a><p>'+disc+'</p><hr>').appendTo('#user-element');
+
+						console.log(title);
+					}
+
+				},
+				error:function() {
+					alert('もう一度試してください。');
 				}
-				
+			});
+		});
+	}
+
+	function clickTag(){
+		$('[data-menu]').removeClass('menu_active');
+		$('[data-menu="tag"]').addClass('menu_active');
+		$('.user-content,.popular-content').css('display','none');
+		$('.tag-content').css('display','block');
+
+		$('#tag-search-btn').click(function(){
+			searchWord = $('#tag-search-word').val();
+			$('#tag-recomend-area').html('');
+			$('#tag-element').html('');
+			$.ajax({
+				type:'GET',
+				url:'http://feeds.pinboard.in/json/t:'+searchWord,
+				dataType:'jsonp',
+				jsonp:'cb',
+				success:function(json){
+
+					for(i=0;i<10;i++){
+						var title = json[i].d,
+						url = json[i].u,
+						disc = json[i].n;
+						$('<div></div>').html('<a href="'+url+'"<h3>'+title+'</h3></a><p>'+disc+'</p><hr>').appendTo('#tag-element');
+
+
+					}
+
+				},
+				error:function() {
+					alert('もう一度試してください。');
+				}
+			});
+		});
+
+	}
+	function clickPopular(){
+		$('[data-menu]').removeClass('menu_active');
+		$('[data-menu="popular"]').addClass('menu_active');
+		$('.user-content,.tag-content').css('display','none');
+		$('.popular-content').css('display','block');
+		$('#popular-element').html('');
+		$.ajax({
+			type:'GET',
+			url:'http://feeds.pinboard.in/json/popular',
+			dataType:'jsonp',
+			jsonp:'cb',
+			success:function(json){
+
+				for(i=0;i<10;i++){
+					var title = json[i].d,
+					url = json[i].u,
+					disc = json[i].n;
+					$('<div></div>').html('<a href="'+url+'"<h3>'+title+'</h3></a><p>'+disc+'</p><hr>').appendTo('#popular-element');
+
+
+				}
+
+			},
+			error:function() {
+				alert('もう一度試してください。');
 			}
-		}else{
-			$('.form-group,#recomend-area').css('display','none');
-			
-		}
+		});
+	}
+
+
+
+
+
+
+	// function switchTab(){
+		
+	// 	console.log(menuType);
+	// 	for(key in tabclicked){
+	// 		tabclicked[key]=false;
+	// 		console.log(tabclicked[key]);
+	// 	}
+	// 	tabclicked[menuType]=true;
+	// 	console.log(tabclicked);
+
+	// }
+
+
+	// tab click
+	$('.menu').click(function(){
+		menuType = $(this).attr('data-menu');
+		if(menuType=='user') clickUser();
+		if(menuType=='tag') clickTag();
+		if(menuType=='popular') clickPopular();
+
+		
+		$(this).addClass('menu_active');
 
 
 		$('#element').html('');
@@ -27,14 +140,15 @@ $(function(){
 			dataType:'jsonp',
 			jsonp:'cb',
 			success:function(json){
-				$('#element').html('');
-				for(i=0;i<10;i++){
-					var title = json[i].d,
-					url = json[i].u,
-					disc = json[i].n;
-					$('<div></div>').html('<a href="'+url+'"<h3>'+title+'</h3></a><p>'+disc+'</p>').appendTo('#element');
+				if(tabclicked[menuType]==true){
+					for(i=0;i<10;i++){
+						var title = json[i].d,
+						url = json[i].u,
+						disc = json[i].n;
+						$('<div></div>').html('<a href="'+url+'"<h3>'+title+'</h3></a><p>'+disc+'</p><hr>').appendTo('#element');
 
-					console.log(title);
+						console.log(title);
+					}
 				}
 			},
 			error:function(XMLHttpRequest, textStatus, errorThrown) {
@@ -74,5 +188,5 @@ $(function(){
 
 	});
 
-	
+
 });
