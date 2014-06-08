457415e1896653861c9c0002fb43a16e01451978
chiiia12 <llnsbchsep12@gmail.com>
2014-06-08 14:18:41 +0900

    add error msg

--- pinborad_app/js/main.js
+++ pinborad_app/js/main.js
@@ -1,93 +1,123 @@
 $(function(){
 	var menuType;
-	var recomendUser = ["studiomohawk","studiomohawk","studiomohawk"],
-	recomendTag = ["javascript","chrome","browser","business"],
-	tabclicked = {'user':false,'tag':false,'popular':false};
-	console.log(tabclicked['user']);
-
+	$('[data-menu="user"]').addClass('menu_active');
 	clickUser();
 
 	function clickUser(){
-		$('[data-menu]').removeClass('menu_active');
-		$('[data-menu="user"]').addClass('menu_active');
 		$('.tag-content,.popular-content').css('display','none');
 		$('.user-content').css('display','block');
 
 		$('#user-search-btn').click(function(){
 			searchWord = $('#user-search-word').val();
-			$('#user-recomend-area').html('');
-			$('#user-element').html('');
-			$.ajax({
-				type:'GET',
-				url:'http://feeds.pinboard.in/json/u:'+searchWord,
-				dataType:'jsonp',
-				jsonp:'cb',
-				success:function(json){
+			if(searchWord!==""){
+			loadUserArticle();
+		}else{
+			$('#user-erorr-msg').html('タグ名を入力してください');
+		}
+		});
 
-					for(i=0;i<10;i++){
-						var title = json[i].d,
-						url = json[i].u,
-						disc = json[i].n;
-						$('<div></div>').html('<a href="'+url+'"<h3>'+title+'</h3></a><p>'+disc+'</p><hr>').appendTo('#user-element');
-
-						console.log(title);
-					}
-
-				},
-				error:function() {
-					alert('もう一度試してください。');
-				}
-			});
+		$('#user-recomend-list > li > a').click(function(){
+			searchWord = $(this).html();
+			$('#user-search-word').val(searchWord);
+			console.log(searchWord);
+			loadUserArticle();
 		});
 	}
 
+	function loadUserArticle(){
+		$('#user-erorr-msg').empty();
+		$('#user-recomend-area').css('display','none');
+		$('#user-element').html('');
+		$('.loading').html('<img src="img/ajax-loader.gif">');
+		$.ajax({
+			type:'GET',
+			url:'http://feeds.pinboard.in/json/u:'+searchWord,
+			dataType:'jsonp',
+			jsonp:'cb',
+			success:function(json){
+				$('.loading').empty();
+				for(i=0;i<10;i++){
+					var title = json[i].d,
+					url = json[i].u,
+					disc = json[i].n;
+					$('<div></div>').html('<a href="'+url+'"<h3>'+title+'</h3></a><p>'+disc+'</p><hr>').appendTo('#user-element');
+
+					console.log(title);
+				}
+
+			},
+			error:function() {
+				alert('もう一度試してください。');
+			}
+		});
+	}
+
+
+
 	function clickTag(){
-		$('[data-menu]').removeClass('menu_active');
-		$('[data-menu="tag"]').addClass('menu_active');
 		$('.user-content,.popular-content').css('display','none');
 		$('.tag-content').css('display','block');
 
 		$('#tag-search-btn').click(function(){
 			searchWord = $('#tag-search-word').val();
-			$('#tag-recomend-area').html('');
-			$('#tag-element').html('');
-			$.ajax({
-				type:'GET',
-				url:'http://feeds.pinboard.in/json/t:'+searchWord,
-				dataType:'jsonp',
-				jsonp:'cb',
-				success:function(json){
-
-					for(i=0;i<10;i++){
-						var title = json[i].d,
-						url = json[i].u,
-						disc = json[i].n;
-						$('<div></div>').html('<a href="'+url+'"<h3>'+title+'</h3></a><p>'+disc+'</p><hr>').appendTo('#tag-element');
-
-
-					}
-
-				},
-				error:function() {
-					alert('もう一度試してください。');
-				}
-			});
+			if(searchWord!==""){
+				loadTagArticle();
+			}else{
+				$('#tag-erorr-msg').html('タグ名を入力してください');
+			}
 		});
 
+		$('#tag-recomend-list > li > a').click(function(){
+			searchWord = $(this).html();
+			$('#tag-search-word').val(searchWord);
+			console.log(searchWord);
+			loadTagArticle();
+		});
+
+
 	}
+
+	function loadTagArticle(){
+		$('#tag-erorr-msg').empty();
+		$('#tag-recomend-area').css('display','none');
+		$('#tag-element').html('');
+		$('.loading').html('<img src="img/ajax-loader.gif">');
+		$.ajax({
+			type:'GET',
+			url:'http://feeds.pinboard.in/json/t:'+searchWord,
+			dataType:'jsonp',
+			jsonp:'cb',
+			success:function(json){
+				$('.loading').empty();
+				for(i=0;i<10;i++){
+					var title = json[i].d,
+					url = json[i].u,
+					disc = json[i].n;
+					$('<div></div>').html('<a href="'+url+'"<h3>'+title+'</h3></a><p>'+disc+'</p><hr>').appendTo('#tag-element');
+
+					console.log(title);
+				}
+
+			},
+			error:function() {
+				alert('もう一度試してください。');
+			}
+		});
+	}
+
+
 	function clickPopular(){
-		$('[data-menu]').removeClass('menu_active');
-		$('[data-menu="popular"]').addClass('menu_active');
 		$('.user-content,.tag-content').css('display','none');
 		$('.popular-content').css('display','block');
 		$('#popular-element').html('');
+		$('.loading').html('<img src="img/ajax-loader.gif">');
 		$.ajax({
 			type:'GET',
 			url:'http://feeds.pinboard.in/json/popular',
 			dataType:'jsonp',
 			jsonp:'cb',
 			success:function(json){
-
+				$('.loading').empty();
 				for(i=0;i<10;i++){
 					var title = json[i].d,
 					url = json[i].u,
@@ -106,87 +136,17 @@ $(function(){
 
 
 
-
-
-
-	// function switchTab(){
-		
-	// 	console.log(menuType);
-	// 	for(key in tabclicked){
-	// 		tabclicked[key]=false;
-	// 		console.log(tabclicked[key]);
-	// 	}
-	// 	tabclicked[menuType]=true;
-	// 	console.log(tabclicked);
-
-	// }
-
-
 	// tab click
 	$('.menu').click(function(){
 		menuType = $(this).attr('data-menu');
+		$('[data-menu]').removeClass('menu_active');
+		$(this).addClass('menu_active');
 		if(menuType=='user') clickUser();
 		if(menuType=='tag') clickTag();
 		if(menuType=='popular') clickPopular();
-
-		
-		$(this).addClass('menu_active');
-
-
-		$('#element').html('');
-		$.ajax({
-			type:'GET',
-			url:'http://feeds.pinboard.in/json/'+menuType,
-			dataType:'jsonp',
-			jsonp:'cb',
-			success:function(json){
-				if(tabclicked[menuType]==true){
-					for(i=0;i<10;i++){
-						var title = json[i].d,
-						url = json[i].u,
-						disc = json[i].n;
-						$('<div></div>').html('<a href="'+url+'"<h3>'+title+'</h3></a><p>'+disc+'</p><hr>').appendTo('#element');
-
-						console.log(title);
-					}
-				}
-			},
-			error:function(XMLHttpRequest, textStatus, errorThrown) {
-				alert('もう一度試してください。');
-			}
-		});
-
-
 	});
 
 
-	$('#search-btn').click(function(){
-		var word = $('#search-word').val();
-		console.log(word);
-
-		$.ajax({
-			type:'GET',
-			url:'http://feeds.pinboard.in/json/'+menuType+':'+word,
-			dataType:'jsonp',
-			jsonp:'cb',
-			success:function(json){
-				$('#element').html('');
-				for(i=0;i<10;i++){
-					var title = json[i].d,
-					url = json[i].u,
-					disc = json[i].n;
-					$('<div></div>').html('<a href="'+url+'"<h3>'+title+'</h3></a><p>'+disc+'</p>').appendTo('#element');
-
-					console.log(title);
-				}
-			},
-			error:function(XMLHttpRequest, textStatus, errorThrown) {
-				alert('もう一度試してください。');
-			}
-		});
-
-
-	});
-
+	
 
 });
